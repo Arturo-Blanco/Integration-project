17526172c3607e6924b7ba5b4db5b7c6f371922d
Arturo Blanco <blancoarturo92@gmail.com>
2023-10-12 19:45:14 -0300

    back functionalities are added

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
+  app.enableCors({
 
   });
   await app.listen(3010);
